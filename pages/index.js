1e49be8fa90515c9f8843e03353e5f391460f617
rahul mathur <rmathur.028@gmail.com>
2023-05-07 01:33:15 +0530

    pagination

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,21 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import Carousel from "@/components/Carousel";
 import MovieBox from "@/components/MovieBox";
+import Pager from "@/components/pager";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const index = () => {
 
-const [list, setList] = useState()
+const [list, setList] = useState();
+let [page, setPage] = useState(1)
 
   useEffect(() => {
   axios
-  .get(`https://api.themoviedb.org/3/movie/top_rated?api_key=f08201c68239a7196fde60ba18a61db3&language=en-US&page=1`)
+  .get(`https://api.themoviedb.org/3/movie/top_rated?api_key=f08201c68239a7196fde60ba18a61db3&language=en-US&page=${page}`)
   .then(({data})=>setList(data.results))
   .catch((err)=>console.log(err))
-  }, [])
+  }, [page])
   
 
 
@@ -23,12 +25,14 @@ const [list, setList] = useState()
     <div>
       <Navbar/>
       <Carousel/>
+       <Pager page={page} setPage={setPage}/>
       <div className="container">
         <div className="moviecard">
          {
           list && list.map((movie)=> <MovieBox list={movie}/>)
          }
         </div>
+        <Pager page={page} setPage={setPage}/>
       </div>
     </div>
   );
